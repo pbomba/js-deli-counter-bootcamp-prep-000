865138947afdbf6a81a981afb824dea87070ca23
Paul Bomba <paul@paulbomba.com>
2018-05-25 16:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var KatzDeli = [];
 var katzDeliLine = [];
 var name;
-var newLine = [];
+let placeInLine = 0;
 
-function takeANumber(array, name){
-  newLine = array.push(name);
-  let tempPlaceInLine = array.indexOf(name);
-  let placeInLine = tempPlaceInLine + 1;
-  return `Welcome, ${name}. You are number ${placeInLine} in line.`;
+
+function takeANumber(array){
+  //return `Welcome, ${name}. You are number ${lineNumber} in line.`;
+  placeInLine = placeInLine + (array.length + 1)
+  let lineNumber = array.push(placeInLine);
+  return placeInLine;
 }
 
-function nowServing(array, name){
+function nowServing(array){
   if (array.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
     let serving = array.shift();
-    return `Currently serving ${serving}.`;
+    return `Currently serving number ${serving}.`;
   }
 }
 
@@ -23,19 +24,15 @@ function currentLine(array){
   if (array.length === 0) {
     return "The line is currently empty.";
   } else {
-    
     let newArray = [];
-    
     for (let i = 0; i < array.length; i++){
       newArray.push(`${i+1}. ${array[i]}`);
     }
-    console.log(newArray)
     let theLine = newArray.join(", ");
-    console.log(theLine);
     return `The line is currently: ${theLine}`;
-
   }
 }
+
 
 
     
